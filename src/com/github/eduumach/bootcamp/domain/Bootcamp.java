342328fd68d358677160cdcb23c780015b2ebf55
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2022-02-01 12:34:26 -0300

    add Bootcamp and business rule

---
+++ src/com/github/eduumach/bootcamp/domain/Bootcamp.java
@@ -0,0 +1,70 @@
+package com.github.eduumach.bootcamp.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String name;
+    private String description;
+    private final LocalDate initialDate = LocalDate.now();
+    private final LocalDate finalDate = initialDate.plusDays(45);
+    private Set<Dev> subscribedDev = new HashSet<>();
+    private Set<Contents> contents = new LinkedHashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getInitialDate() {
+        return initialDate;
+    }
+
+    public LocalDate getFinalDate() {
+        return finalDate;
+    }
+
+    public Set<Dev> getSubscribedDev() {
+        return subscribedDev;
+    }
+
+    public void setSubscribedDev(Set<Dev> subscribedDev) {
+        this.subscribedDev = subscribedDev;
+    }
+
+    public Set<Contents> getContents() {
+        return contents;
+    }
+
+    public void setContents(Set<Contents> contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(name, bootcamp.name) && Objects.equals(description, bootcamp.description) && Objects.equals(initialDate, bootcamp.initialDate) && Objects.equals(finalDate, bootcamp.finalDate) && Objects.equals(subscribedDev, bootcamp.subscribedDev) && Objects.equals(contents, bootcamp.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, initialDate, finalDate, subscribedDev, contents);
+    }
+}
